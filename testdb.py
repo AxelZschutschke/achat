004e9363e82b19e80fe7b212dcbda39c19ad276e
Axel Zschutschke <axel@4e179b62-8137-4299-8b62-b10feb66a947.fritz.box>
2025-08-18 21:54:20 +0200

    removed outdated test

--- testdb.py
+++ testdb.py
@@ -1,20 +1,8 @@
+from chat.db import DB
 from chat.backendOllama import BackendOllama
-from db.db import DB
-from db.tools import createDB, loadDB
-import glob
+from db.embedding import *
 
-folders = [".", "chat", "db", "mcp"]
-filter = "*.py"
-files = []
-for f in folders:
-    files += [
-        f"{f}/{x}" for x in glob.glob(pathname=filter, root_dir=f,recursive=True)
-        ]
-print(files)
-
-
-db = createDB("tmp/rag.db", files, backend=BackendOllama())
-print(db.search("removeMessageFromHistory",1))
-
-#db2 = loadDB("tmp/database.db",backend=BackendOllama())
-#print(db2.search("cannons shoot balls",1,0))
+vdb = DB(BackendOllama(), float16)
+vdb.open("tmp/db",".md",640,512)
+q = vdb.embed("brown leather")
+print(vdb.search(q,1,length=2048, debug=True))
